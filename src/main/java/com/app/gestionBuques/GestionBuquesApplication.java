501aded52fcead650f17bf98b7616b8e3e0b2c4b
J0SE <joseandrestorres0508@gmail.com>
2025-03-17 18:38:16 -0500

    feat: Primer Commit

---
+++ src/main/java/com/app/gestionBuques/GestionBuquesApplication.java
@@ -0,0 +1,13 @@
+package com.app.gestionBuques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBuquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionBuquesApplication.class, args);
+	}
+
+}
